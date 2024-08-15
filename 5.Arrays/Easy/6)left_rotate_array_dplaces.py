d4209c92797a6a57b6699a4d68655637ff2ed5f6
Priyanshu Sakre <priyanshusakre30@gmail.com>
2024-08-15 23:44:35 +0530

    sort

--- 5.Arrays/Easy/6)left_rotate_array_dplaces.py
+++ 5.Arrays/Easy/6)left_rotate_array_dplaces.py
@@ -26,3 +26,22 @@ def rotate_array2(n, arr, k):
 arr2 = [1, 2, 3, 4, 5]
 n = len(arr2)
 print(rotate_array2(n, arr2, 3))
+
+
+# reverse fn
+def reverse1(arr, start, end):
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1
+        end -= 1
+
+
+def rotate_array3( arr, k):
+    reverse1(arr, 0, k - 1)
+    reverse1(arr, k, len(arr) - 1)
+    reverse1(arr, 0, len(arr) - 1)
+    return arr
+
+
+arr3 = [1, 2, 3, 4, 5, 6, 7]
+print(rotate_array3(arr3, 3))
