f5d03f047603ae4a5db4cfb9868fb2fad2c1d702
Priyanshu Sakre <priyanshusakre30@gmail.com>
2024-08-15 12:02:15 +0530

    Bubble

--- 4.Sorting/bubble_sort.py
+++ 4.Sorting/bubble_sort.py
@@ -3,9 +3,9 @@ l1 = [64, 25, 12, 22, 11]
 
 def bubble_sort(l1):
     n = len(l1)
-    for i in range(n):
+    for i in range(n):  # also use range(n-1,0,-1)
         swap = False  # to optimize the algorithm
-        for j in range(n - i - 1):
+        for j in range(n - i - 1):  # also use range(0,i-1)
             if l1[j] > l1[j + 1]:
                 l1[j], l1[j + 1] = l1[j + 1], l1[j]
                 swap = True
