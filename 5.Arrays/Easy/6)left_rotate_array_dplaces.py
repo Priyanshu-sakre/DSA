8431732fd9dc61e7440621cc4f40c86b0716ee2e
Priyanshu Sakre <priyanshusakre30@gmail.com>
2024-08-15 22:04:45 +0530

    1

---
+++ 5.Arrays/Easy/6)left_rotate_array_dplaces.py
@@ -0,0 +1,28 @@
+# less time
+def rotate_array(n, arr, k):
+    temp = arr[0:k]
+    for i in range(k, n):
+        arr[i - k] = arr[i]
+    for i in range(n - k, n):
+        arr[i] = temp[i - (n - k)]
+    return arr
+
+
+arr = [1, 2, 3, 4, 5]
+n = len(arr)
+print(rotate_array(n, arr, 3))
+
+
+# more time
+def rotate_array2(n, arr, k):
+    for i in range(k):
+        temp = arr[0]
+        for i in range(1, n):
+            arr[i - 1] = arr[i]
+        arr[n - 1] = temp
+    return arr
+
+
+arr2 = [1, 2, 3, 4, 5]
+n = len(arr2)
+print(rotate_array2(n, arr2, 3))
