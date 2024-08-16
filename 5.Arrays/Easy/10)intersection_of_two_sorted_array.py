5184a9221fb2b7adf484ae6db007af2bb4a97746
Priyanshu Sakre <priyanshusakre30@gmail.com>
2024-08-16 17:09:25 +0530

    Intersection

---
+++ 5.Arrays/Easy/10)intersection_of_two_sorted_array.py
@@ -0,0 +1,41 @@
+l1 = [1, 2, 2, 3, 3, 4, 5, 6]
+l2 = [2, 3, 3, 5, 6, 6, 7]
+n = len(l1)
+m = len(l2)
+
+
+# brute
+def intersection(l1, l2, n, m):
+    visit = [0] * m
+    intersect = []
+    for i in range(n):
+        for j in range(m):
+            if l1[i] == l2[j] and visit[j] == 0:
+                intersect.append(l1[i])
+                visit[j] = 1
+                break
+            if l2[j] > l1[i]:
+                break
+    return intersect
+
+
+print(intersection(l1, l2, n, m))
+
+
+# optimal
+def intersection2(l1, l2, n, m):
+    intersect2 = []
+    i = j = 0
+    while i < n and j < m:
+        if l1[i] < l2[j]:
+            i += 1
+        elif l1[i] > l2[j]:
+            j += 1
+        else:
+            intersect2.append(l1[i])
+            i += 1
+            j += 1
+    return intersect2
+
+
+print(intersection2(l1, l2, n, m))
