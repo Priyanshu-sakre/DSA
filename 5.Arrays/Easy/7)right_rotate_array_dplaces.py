7c59be4aa85119770993049080bb674e47485f60
Priyanshu Sakre <priyanshusakre30@gmail.com>
2024-08-17 12:39:42 +0530

    some minor tweeks

--- 5.Arrays/Easy/7)right_rotate_array_dplaces.py
+++ 5.Arrays/Easy/7)right_rotate_array_dplaces.py
@@ -1,3 +1,19 @@
+# brute
+def rotate_array(arr, k):
+    for i in range(k):
+        n = len(arr)
+        temp = arr[n - 1]
+        for i in range(n - 1, 0, -1):
+            arr[i] = arr[i - 1]
+        arr[0] = temp
+    return arr
+
+
+arr2 = [1, 2, 3, 4, 5, 6, 7]
+print(rotate_array(arr2, 3))
+
+
+# using reverse function
 def reverse1(arr, start, end):
     while start < end:
         arr[start], arr[end] = arr[end], arr[start]
