6b8695542bbfc0014eaaf74300aaf456d110d182
Priyanshu Sakre <priyanshusakre30@gmail.com>
2024-08-17 18:43:08 +0530

    Longest subarray

--- 5.Arrays/Easy/14)longest_subarray.py
+++ 5.Arrays/Easy/14)longest_subarray.py
@@ -3,6 +3,7 @@ n = len(arr)
 k = 3
 
 
+# brute
 def longest_subarray(arr, n, k):
     l1 = 0
     for i in range(n):
@@ -14,4 +15,57 @@ def longest_subarray(arr, n, k):
     return l1
 
 
+"""
+TC->O(N^2)
+SC->O(1)
+"""
+
+
 print(longest_subarray(arr, n, k))
+
+
+# better
+def longest_subarray2(arr, n, k):
+    hash_map = {}
+    sum1 = 0
+    maxx = 0
+    for i in range(n):
+        sum1 += arr[i]
+        if sum1 == k:
+            maxx = i + 1
+
+        rem = sum1 - k
+        if rem in hash_map:
+            l = i - hash_map[rem]
+            maxx = max(l, maxx)
+        if sum1 not in hash_map:
+            hash_map[sum1] = i
+    return maxx
+
+
+print(longest_subarray2(arr, n, k))
+
+"""
+TC->O(N)
+SC->O(N)
+"""
+
+
+# optimal
+def longest_subarray3(arr, n, k):
+    sum1 = arr[0]
+    i = j = 0
+    maxL = 0
+    while j < n:
+        while sum1 > k:
+            sum1 -= arr[i]
+            i += 1
+        if sum1 == k:
+            maxL = max(maxL, j - i + 1)
+        j += 1
+        if j < n:
+            sum1 += arr[j]
+    return maxL
+
+
+print(longest_subarray3(arr, n, k))
