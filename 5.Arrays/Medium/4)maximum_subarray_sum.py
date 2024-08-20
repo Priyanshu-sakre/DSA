e71d329da8a979fb43028afe79a3d644899509e3
Priyanshu Sakre <priyanshusakre30@gmail.com>
2024-08-21 00:15:34 +0530

    1

--- 5.Arrays/Medium/4)maximum_subarray_sum.py
+++ 5.Arrays/Medium/4)maximum_subarray_sum.py
@@ -2,6 +2,24 @@
 def maxSubArray(nums):
     sum_final = nums[0]
     for i in range(len(nums)):
+        for j in range(i, len(nums)):
+            sum1 = 0
+            for k in range(i, j + 1):
+                sum1 += nums[k]
+                sum_final = max(sum1, sum_final)
+    return sum_final
+
+
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(maxSubArray(nums))
+
+
+# better
+
+
+def maxSubArray2(nums):
+    sum_final = nums[0]
+    for i in range(len(nums)):
         sum2 = 0
         for j in range(i, len(nums)):
             sum2 += nums[j]
@@ -11,16 +29,20 @@ def maxSubArray(nums):
 
 nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 print(maxSubArray(nums))
+
 # optimal
+
 """Kadane's Algorithm"""
 
 
-def maxSubArray2(nums):
+def maxSubArray3(nums):
     sum1 = 0
     max1 = nums[0]
     for i in nums:
         sum1 += i
-        max1 = max(max1, sum1)
+        # max1 = max(max1, sum1) (either of them can be used)
+        if sum1 > max1:
+            max1 = sum1
         if sum1 < 0:
             sum1 = 0
     return max1
