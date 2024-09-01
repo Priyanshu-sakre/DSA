f5b7d18ecb8190ad2bba57703cdea698c98c97b4
Priyanshu Sakre <priyanshusakre30@gmail.com>
2024-09-01 22:32:58 +0530

    Few tweeks

--- 5.Arrays/Easy/2)second_largest.py
+++ 5.Arrays/Easy/2)second_largest.py
@@ -1,10 +1,10 @@
-arr = [12, 35, 1, 10, 34, 1]
+arr = [12, 35, 1, 10, 34, 1, 99]
 largest = arr[0]
 sec_largest = 0
 for i in arr:
-    if i > largest and i > sec_largest:
+    if i > largest:
         sec_largest = largest
         largest = i
-    elif i < largest and i > sec_largest:
+    elif i > sec_largest:
         sec_largest = i
 print(sec_largest)
