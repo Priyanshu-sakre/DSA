9b2b57a0e1770a36f9b4aa4ab33943a260db5b34
Priyanshu Sakre <priyanshusakre30@gmail.com>
2024-09-27 23:34:35 +0530

    dll

---
+++ 8.Linked List/DLL/2)array_to_dll.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, data):  # data -> value stored in node
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+class Solution:
+    def constructDLL(self, arr):
+        head = Node(arr[0])
+        mover = head
+        for i in range(1, len(arr)):
+            temp = Node(arr[i])
+            mover.next = temp
+            temp.prev = mover
+            mover = mover.next
+        current = head
+        while current:
+            print(current.data, end=" ")
+            current = current.next
