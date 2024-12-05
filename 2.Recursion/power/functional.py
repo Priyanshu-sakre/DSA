cc1fe600e1a03c040c5ced63d9adcecf0eb04e35
Priyanshu Sakre <priyanshusakre30@gmail.com>
2024-12-05 17:33:00 +0530

    power n//2

--- 2.Recursion/power/functional.py
+++ 2.Recursion/power/functional.py
@@ -1,3 +1,4 @@
+# brute
 def power1(n, b):
     if b == 0:
         return 1
@@ -6,3 +7,21 @@ def power1(n, b):
 
 
 print(power1(5, 3))
+
+
+# better
+def power2(n, b):
+    if b == 0:
+        return 1
+    if b < 0:
+        n = 1 / n
+        b = -b
+    temp = power2(n, b // 2)
+    temp = temp * temp
+    if b % 2 == 0:
+        return temp
+    else:
+        return temp * n
+
+
+print(power2(2, -2))
